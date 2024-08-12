b604cc86f1fffa08c341eed2a26006e62c20f148
Pravin <pravindaware9221@gmail.com>
2024-08-12 14:24:21 +0530

    code file and img added

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function move(num){
+const Img = document.getElementById("main-img")
+ if(num == 1){
+    Img.src="img/1.jpg"
+ }
+ else if (num == 2){
+    Img.src="img/2.jpg"
+ }
+ else if (num == 3){
+    Img.src="img/3.jpg"
+ }
+ else if (num == 4){
+    Img.src="img/4.jpg"
+ }
+ else if (num == 5){
+    Img.src="img/5.jpg"
+ }
+ else if (num == 6){
+    Img.src="img/6.jpg"
+ }
+ else{
+    
+ }
+
+}
